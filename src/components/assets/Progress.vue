368427bdb3e47fb1207d37362d1d562283653b8a
Hüseyin Yalçın <kaguwomin@gmail.com>
2021-06-05 01:00:01 +0300

    Update Progress.vue

--- src/components/assets/Progress.vue
+++ src/components/assets/Progress.vue
@@ -11,79 +11,69 @@
   </div>
 
   <div class="mt-2 flex items-center justify-between text-black @dark:text-white">
-    <span>{{ getTime.elapsed }}</span>
-    <span>{{ getTime.left }}</span>
+    <span>{{ getTimeElapsed }}</span>
+    <span>{{ getTimeLeft }}</span>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, defineProps, onMounted, ref } from "vue";
+import { useTimestamp } from "@vueuse/core"
+import { computed, defineProps } from "vue"
 
 const props = defineProps({
   start: {
     type: Number,
     required: true,
-    default: 0,
+    default: 0
   },
   end: {
     type: Number,
     required: true,
-    default: 0,
-  },
-});
+    default: 0
+  }
+})
 
 // Use a reference to generate reactive time.
-const time = ref(new Date().getTime());
+const { timestamp } = useTimestamp()
 
-// Updated reactive time every 100 milliseconds.
-onMounted(() => {
-  setInterval(() => {
-    time.value = new Date().getTime();
-  }, 100);
-});
-
-/**
- * Returns elapsed and left time information.
- */
-const getTime = computed(() => {
-  if (props.start === 0 && props.end === 0)
-    return { elapsed: "00:00", left: "00:00" };
-
-  const timeElapsed = time.value - props.start;
-  const timeLeft = props.end - time.value;
+// Computed methods
+const getTimeElapsed = computed(() => {
+  const currentTime = timestamp.value
+  const timeElapsed = currentTime - props.start
 
   const timeElapsedArray = [
     Math.round((timeElapsed / (1000 * 60)) % 60),
-    Math.round((timeElapsed / 1000) % 60),
-  ];
+    Math.round((timeElapsed / 1000) % 60)
+  ]
+
+  const mapFunction = (time: number) => `0${time}`.slice(-2)
+  return timeElapsedArray.map(mapFunction).join(":")
+})
+
+const getTimeLeft = computed(() => {
+  const currentTime = timestamp.value
+  const timeLeft = props.end - (props.start || currentTime)
 
   const timeLeftArray = [
     Math.round((timeLeft / (1000 * 60)) % 60),
-    Math.round((timeLeft / 1000) % 60),
-  ];
+    Math.round((timeLeft / 1000) % 60)
+  ]
 
-  const mapFunction = (time: number) => `0${time}`.slice(-2);
+  const mapFunction = (time: number) => `0${time}`.slice(-2)
+  return timeLeftArray.map(mapFunction).join(":")
+})
 
-  return {
-    elapsed: timeElapsedArray.map(mapFunction).join(":"),
-    left: timeLeftArray.map(mapFunction).join(":"),
-  };
-});
-
-/**
- * Returns the width percentage of progress bar.
- */
 const getStyles = computed(() => {
-  const total = props.end - props.start;
-  const progress = 100 - (100 * (props.end - time.value)) / total;
+  const total = props.end - props.start
+  const progress = 100 - (100 * (props.end - timestamp.value)) / total
 
   if (progress > 100)
     return {
-      width: "100%",
-    };
+      width: "100%"
+    }
   else
     return {
-      width: `${progress.toFixed(2)}%`,
-    };
-});
+      width: `${progress.toFixed(2)}%`
+    }
+})
 </script>
