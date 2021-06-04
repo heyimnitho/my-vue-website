f7f4faabd257be3d531799925f014942e387532c
Hüseyin Yalçın <kaguwomin@gmail.com>
2021-06-05 01:25:30 +0300

    Update Navbar.vue

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -3,20 +3,22 @@
         <header class="text-gray-800 @dark:text-white body-font">
   <div class="container mx-auto flex flex-wrap p-7 flex-col md:flex-row items-start">
     <a class="flex title-font font-medium items-center text-black mb-6 md:mb-0">
-        <!-- 
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-blue-600 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-        -->
       <span class="ml-2 text-xl @dark:text-white hover:underline" draggable="false" href="/">nitho.ga</span>
     </a>
     <nav class="ml-auto md:mr-auto flex flex-initial flex-wrap items-start text-start text-lg justify-start">
-      <a class="-mx-8 mr-6 py-2 sm:py-1 sm:px-2 hover:text-gray-900 hover:bg-gray-200 rounded-lg hover:underline hover:underline-dashed transition-all duration-150" href="https://nitho.medium.com/">Medium</a>
-      <a class="mr-7 py-2 sm:py-1 sm:px-2 hover:text-gray-900 hover:bg-gray-200 rounded-lg hover:underline hover:underline-dashed transition-all duration-150" href="https://github.com/heyimnitho?tab=repositories">Repositories</a>
-      <a class="-mx-1 mr-2 py-2 sm:py-1 sm:px-2 hover:text-gray-900 hover:bg-gray-200 rounded-lg hover:underline hover:underline-dashed transition-all duration-150" href="https://open.spotify.com/user/i1ftnp8o2z994a72zglh311rn">Spotify</a>
+      <a class="-mx-8 mr-6 py-2 sm:py-1 sm:px-2 hover:text-gray-900 hover:bg-gray-200 rounded-lg transition-all duration-150" href="https://nitho.medium.com/">Medium</a>
+      <a class="mr-7 py-2 sm:py-1 sm:px-2 hover:text-gray-900 hover:bg-gray-200 rounded-lg transition-all duration-150" href="https://github.com/heyimnitho?tab=repositories">Repositories</a>
+      <a class="-mx-1 mr-2 py-2 sm:py-1 sm:px-2 hover:text-gray-900 hover:bg-gray-200 rounded-lg transition-all duration-150" href="https://open.spotify.com/user/i1ftnp8o2z994a72zglh311rn">Spotify</a>
     </nav>
     <span class="border-0 py-1 px-3 mt-4 md:mt-0 inline-flex items-center w-4 h-4 ml-1"/>
   </div>
 </header>
     </div>
 </template>
+
+
+        <!-- 
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-blue-600 rounded-full" viewBox="0 0 24 24">
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+      </svg>
+        -->
