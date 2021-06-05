4430a20feaa67e72395144c2d03c8e3e51c9a8f5
Hüseyin Yalçın <kaguwomin@gmail.com>
2021-06-05 12:54:19 +0300

    Update Footer.vue

--- src/Footer.vue
+++ src/Footer.vue
@@ -7,7 +7,7 @@
                 <p class="leading-relaxed text-base @dark:text-white text-black">
                     Most of the components are taken from GitHub and other template websites. I'm gonna give all of the links that I tried to create this portfolio for everyone's usage. Want to contribute this project? Send your GitHub pull request to repository and let me check every details of your changed/added assets to website.
                 </p>
-                <a class="text-violet-400 inline-flex items-center mt-4 mb-6 hover:underline" href="https://github.com/heyimnitho/website"> GitHub Repository
+                <a class="text-purple-600 @dark:text-violet-400 inline-flex items-center mt-4 mb-6 hover:underline" href="https://github.com/heyimnitho/website"> GitHub Repository
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
@@ -17,17 +17,17 @@
                 <h2 class="title-font font-medium text-black @dark:text-white tracking-widest text-md mb-3">ASSETS FROM OTHER CREATORS</h2>
                 <nav class="flex flex-wrap list-none -mb-1">
                     <li class="lg:w-1/2 mb-1 w-1/2">
-                        <a class="@dark:hover:text-white hover:text-gray-700 transition-all duration-150" href="https://github.com/BraydenTW/react-tailwind-portfolio/">Brayden W. Card Design</a>
+                        <a class="@dark:hover:text-white hover:text-gray-900 text-warm-gray-600 transition-all duration-150" href="https://github.com/BraydenTW/react-tailwind-portfolio/">Brayden W. Card Design</a>
                     </li>
                     <hr />
                     <li class="lg:w-1/2 mb-0.5 xl:-ml-20 w-1/2">
-                        <a class="@dark:hover:text-white hover:text-gray-700 transition-all duration-150" href="https://tailblocks.cc/">Skills & Footer Design</a>
+                        <a class="@dark:hover:text-white hover:text-gray-900 text-warm-gray-600 transition-all duration-150" href="https://tailblocks.cc/">Skills & Footer Design</a>
                     </li>
                     <li class="lg:w-1/2 mb-0.5 w-1/3">
-                        <a class="@dark:hover:text-white hover:text-gray-700 transition-all duration-150" href="https://github.com/eggsy/lanyard-visualizer">EGGSY's Lanyard Visualizer</a>
+                        <a class="@dark:hover:text-white hover:text-gray-900 text-warm-gray-600 transition-all duration-150" href="https://github.com/eggsy/lanyard-visualizer">EGGSY's Lanyard Visualizer</a>
                     </li>
                     <li class="lg:w-1/2 mb-1 xl:-ml-18 w-1">
-                        <a class="@dark:hover:text-white hover:text-gray-700 transition-all duration-150" href="https://icones.js.org/">icònes.js.org</a>
+                        <a class="@dark:hover:text-white hover:text-gray-900 text-warm-gray-600 transition-all duration-150" href="https://icones.js.org/">icònes.js.org</a>
                     </li>
                 </nav>
             </div>
